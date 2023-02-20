09909b4ae694fc6f6a5fbac32c7e0cb425d73b51
Álvaro Úbeda <80052630+alvuberui@users.noreply.github.com>
2023-02-20 17:27:22 +0100

    Initial project

---
+++ src/main/java/com/ispp/heartforchange/HeartforchangeApplication.java
@@ -0,0 +1,13 @@
+package com.ispp.heartforchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartforchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartforchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ispp/heartforchange/HeartforchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ispp.heartforchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartforchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
