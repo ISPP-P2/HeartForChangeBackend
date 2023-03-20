cd26fc887918a074479739e88915f87aec76e8fd
Álvaro Úbeda <80052630+alvuberui@users.noreply.github.com>
2023-03-20 20:14:19 +0100

    Fix url cors

--- src/main/java/com/ispp/heartforchange/HeartforchangeApplication.java
+++ src/main/java/com/ispp/heartforchange/HeartforchangeApplication.java
@@ -20,7 +20,7 @@ public class HeartforchangeApplication {
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:5173/", "http://34.70.27.83/");
+				registry.addMapping("/**").allowedOrigins("http://localhost:5173/**", "http://34.70.27.83/**");
 			}
 		};
 	}
